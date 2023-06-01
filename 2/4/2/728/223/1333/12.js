8222deb2efc5370754650eb972f7cca445a997d3
Ø Û S S Ā M Ā <104790894+OS-OFC@users.noreply.github.com>
2023-06-01 18:33:26 +0100

    Update 12.js

--- 2/4/2/728/223/1333/12.js
+++ 2/4/2/728/223/1333/12.js
@@ -4,6 +4,6 @@
   "apackname": "Sticker by",
   "aauthor": "© Morocco OpenAI",
   "abotname": "Morocco OpemAI Bot",
-  "amenu": "مرحبا بكم في \n *Morocco OpenAI Bot* 🖲️ \n\n *البوت مخصص للتنزيل من هذه المواقع* 🌐 :\n•facebook\n •instagram\n•twitter\n•tiktok\n•youtube\n•mediafire\n•google drive\n\n *كيفية التنزيل* 📥 :\nفقط ارسل رابط يخص هذه المواقع اعلاه وسيتم التنزيل تلقائيا بدون امر 5✅\n\n *كما يدعم ميزة الملصقات*: ارسل صورة او فيديو وسيتحول تلقائيا 🙃لملصق",
+  "amenu": "مرحبا بكم في \n *Morocco OpenAI Bot* 🖲️ \n\n اول بوت مغربي عربي يقدم ميزة الخدمات 🤭التلقائية\n\n *البوت مخصص للتنزيل من هذه المواقع* 🌐 :\n•facebook\n •instagram\n•twitter\n•tiktok\n•youtube\n•mediafire\n•google drive\n\n *كيفية التنزيل* 📥 :\nفقط ارسل رابط يخص هذه المواقع اعلاه وسيتم التنزيل تلقائيا بدون امر 5✅\n\n *كما يدعم ميزة الملصقات*: ارسل صورة او فيديو وسيتحول تلقائيا 🙃لملصق",
   "acaption": "تم التحميل ✅"
 }
