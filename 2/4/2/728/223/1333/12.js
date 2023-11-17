0b16362453d60a6d56a510fdcf74040949f6093a
Ø Û S S Ā M Ā <104790894+OS-OFC@users.noreply.github.com>
2023-11-17 10:10:13 +0100

    Update 12.js

--- 2/4/2/728/223/1333/12.js
+++ 2/4/2/728/223/1333/12.js
@@ -10,7 +10,7 @@
   "apackname": "© Morocco OpenAI",
   "alwasf": "أنا إيفل، روبوت الدردشة الودود الخاص بك. أنا هنا لمساعدتك في الإجابة على الأسئلة، وتلخيص النصوص، أو إنشاء القصائد. أقبل فقط المدخلات النصية.",
   "Modeai": 200,
-  "Modeai2": 200,
+  "Modeai2": 400,
   "Msganime": "*انتظر قليلا ، قد يستغرق بعض الوقت* ......... \n\nشاهد هذا الفيديو اذا كنت تريد صنع مثل هذا البوت_:\nhttps://www.facebook.com/100090780515885/posts/pfbid02TifnuA3AXQdRbu8AbZbriJ32xk44ouagMjbi58DBv54Ea5KUr9maBXSdCQjGsf4Bl/?app=fbl",
   "abotname": "Morocco AI Bot",
     "alink": "https://chat.whatsapp.com/E4NLZ6Tvi8x1B243xNUwEY",
